ea549f7a92681cd3190d1941f2f8a0937d0a67e1
anandarizkyfebriyana <124749589+anandarizkyfebriyana@users.noreply.github.com>
2023-02-12 17:14:00 +0700

    Create hello.java

---
+++ hello.java
@@ -0,0 +1,7 @@
+echo "# E41222236_Ananda-Rizky-Febriyana_E" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/anandarizkyfebriyana/E41222236_Ananda-Rizky-Febriyana_E.git
+git push -u origin main
